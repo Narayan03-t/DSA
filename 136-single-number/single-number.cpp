e32e1e1c3690ae3d3830b5259a98c347dcc307ea
Narayan03-t <152596467+Narayan03-t@users.noreply.github.com>
2025-06-29 05:07:38 +0530

    Time: 4 ms (23.39%) | Memory: 23.8 MB (12.63%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,12 +1,35 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n = nums.size();
-        int ans = 0;
 
-        for(int i=0;i<n;i++){
-            ans = nums[i] ^ ans;
+        // method 1 (XOR)
+        // int n = nums.size();
+        // int ans = 0;
+
+        // for(int i=0;i<n;i++){
+        //     ans = nums[i] ^ ans;
+        // }
+        // return ans;
+
+
+        // method 2 (hashmap)
+        
+        unordered_map<int,int>freqmap;
+        for(int i=0;i<nums.size();i++){
+            int num = nums[i];
+            freqmap[num] = freqmap[num] + 1;
         }
+        int ans;
+        for(auto it : freqmap){
+            int key = it.first;
+            int freq = it.second;
+
+            if(freq == 1){
+            ans = key;
+            break;
+        }
+        }
+        
         return ans;
     }
 };

a8e24535c3010ba9b3e7b749f5adec61a530aee5
Narayan03-t <152596467+Narayan03-t@users.noreply.github.com>
2025-06-30 01:19:13 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (57.39%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -2,22 +2,38 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        int shift = k%n;
+        k = k % n;
 
-        int temp[100000];
-        int index = 0;
-        for(int i=n-shift;i<n;i++){
-            temp[index] = nums[i];
-            index++;
-        } 
+        // 1 reverse all
+        reverse(nums.begin(),nums.end());
 
-        for(int i=n-1;i>=shift;i--){
-            nums[i] = nums[i-shift];
-        }
+        // 2 reverse first k
+        reverse(nums.begin(),nums.begin()+k);
 
-        for(int i=0;i<shift;i++){
-            nums[i] = temp[i];
+        // 3 reverse n-k to n
+        reverse(nums.begin()+k,nums.end());
 
-        }
+
+
+
+
+        // int n = nums.size();
+        // int shift = k%n;
+
+        // int temp[100000];
+        // int index = 0;
+        // for(int i=n-shift;i<n;i++){
+        //     temp[index] = nums[i];
+        //     index++;
+        // } 
+
+        // for(int i=n-1;i>=shift;i--){
+        //     nums[i] = nums[i-shift];
+        // }
+
+        // for(int i=0;i<shift;i++){
+        //     nums[i] = temp[i];
+
+        // }
     }
 };

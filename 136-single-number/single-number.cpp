458958bf79dd8f0b645d298a082cb4093c7cfc77
Narayan03-t <152596467+Narayan03-t@users.noreply.github.com>
2025-06-29 05:44:29 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (98.05%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -12,24 +12,40 @@ public:
         // return ans;
 
 
-        // method 2 (hashmap)
+        // method 3 (hashmap)
         
-        unordered_map<int,int>freqmap;
-        for(int i=0;i<nums.size();i++){
-            int num = nums[i];
-            freqmap[num] = freqmap[num] + 1;
-        }
-        int ans;
-        for(auto it : freqmap){
-            int key = it.first;
-            int freq = it.second;
+        // unordered_map<int,int>freqmap;
+        // for(int i=0;i<nums.size();i++){
+        //     int num = nums[i];
+        //     freqmap[num] = freqmap[num] + 1;
+        // }
+        // int ans;
+        // for(auto it : freqmap){
+        //     int key = it.first;
+        //     int freq = it.second;
 
-            if(freq == 1){
-            ans = key;
-            break;
-        }
-        }
+        //     if(freq == 1){
+        //     ans = key;
+        //     break;
+        // }
+        // }
         
+        // return ans;
+
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+
+        int ans;
+        for(int i=0;i<n;i=i+2){
+            if(i+1<n && nums[i] == nums[i+1]){
+               
+            }
+            else{
+                ans = nums[i];
+                break;
+            }
+        }
         return ans;
+
     }
 };

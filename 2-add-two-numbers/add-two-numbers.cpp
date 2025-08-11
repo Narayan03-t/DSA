e75ac025d8cfaf86d8894729ef74d8a87d695c1c
Tek Narayan Yadav <152596467+Narayan03-t@users.noreply.github.com>
2025-08-12 02:26:41 +0530

    Time: 2 ms (39.48%) | Memory: 77.2 MB (43.67%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -36,7 +36,25 @@ public:
         
     }
 
+    ListNode* recursive(ListNode* l1, ListNode* l2, int carry=0){
+        if(l1==NULL && l2==NULL && carry==0){
+            return 0;
+        }
+        
+        int a = l1 ? l1->val : 0;
+        int b = l2 ? l2->val : 0;
+        int sum = a+b+carry;
+        int digit = sum%10;
+        carry = sum/10;
+
+        ListNode* ans = new ListNode(digit);
+        ans->next = recursive(l1 ? l1->next :l1, l2 ? l2->next : l2,carry);
+
+        return ans;
+    }
+
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        return iterative(l1, l2);
+        // return iterative(l1, l2);
+        return recursive(l1,l2);
     }
 };

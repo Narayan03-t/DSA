2d9afbd44e037bec9325e625400cdb84bcc33d87
Tek Narayan Yadav <152596467+Narayan03-t@users.noreply.github.com>
2025-08-20 18:05:05 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (50.77%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -3,7 +3,7 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int n = nums.size();
 
-        // two pointer approach to solve thi prblm
+        // two pointer approach to solve this prblm
 
         int i=1;
         int j=0;

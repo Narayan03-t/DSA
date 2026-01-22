acad586a77ed2b81c1879c994e2ae3e561aae3f8
Tek Narayan Yadav <152596467+Narayan03-t@users.noreply.github.com>
2026-01-22 10:21:51 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (24.87%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,51 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-
-        // method 1 (XOR)
-        // int n = nums.size();
-        // int ans = 0;
-
-        // for(int i=0;i<n;i++){
-        //     ans = nums[i] ^ ans;
-        // }
-        // return ans;
-
-
-        // method 3 (hashmap)
-        
-        // unordered_map<int,int>freqmap;
-        // for(int i=0;i<nums.size();i++){
-        //     int num = nums[i];
-        //     freqmap[num] = freqmap[num] + 1;
-        // }
-        // int ans;
-        // for(auto it : freqmap){
-        //     int key = it.first;
-        //     int freq = it.second;
-
-        //     if(freq == 1){
-        //     ans = key;
-        //     break;
-        // }
-        // }
-        
-        // return ans;
-
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-
-        int ans;
-        for(int i=0;i<n;i=i+2){
-            if(i+1<n && nums[i] == nums[i+1]){
-               
-            }
-            else{
-                ans = nums[i];
-                break;
-            }
+        int n=nums.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans = ans ^ nums[i];
         }
         return ans;
-
     }
 };

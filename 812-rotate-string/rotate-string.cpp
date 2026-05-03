bea5734f8a6996d759e3acd6fd82c7ff2e96641a
Tek Narayan Yadav <152596467+Narayan03-t@users.noreply.github.com>
2026-05-04 01:49:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (61.65%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -2,16 +2,27 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         // Bruteforce
+        // int n=s.size();
+        // int m=goal.size();
+
+        // if(n != m) return false;
+
+        // for(int i=0;i<n;i++){
+        //     rotate(s.begin(),s.begin()+1,s.end());
+        //     if(s==goal){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        // optimized
         int n=s.size();
         int m=goal.size();
+        if(n!=m) return false;
 
-        if(n != m) return false;
-
-        for(int i=0;i<n;i++){
-            rotate(s.begin(),s.begin()+1,s.end());
-            if(s==goal){
-                return true;
-            }
+        string con = s+s;
+        if(con.find(goal) != string::npos){
+            return true;
         }
         return false;
 

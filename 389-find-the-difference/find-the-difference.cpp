a31222a58728013f4eb716817a5ca6004bdadaae
Tek Narayan Yadav <152596467+Narayan03-t@users.noreply.github.com>
2026-05-29 06:58:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (49.04%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int sum1=0;
-        for(int i=0;i<s.length();i++){
-            char ch = s[i];
-            sum1 = sum1+int(ch);
-        }
-        int sum2=0;
-        for(int i=0;i<t.length();i++){
-            char chh=t[i];
-            sum2=sum2+char(chh);
-        }
-        int ans = sum2-sum1;
+        // Approach 1 (sum approach)
+        // int sum1=0;
+        // for(int i=0;i<s.length();i++){
+        //     char ch = s[i];
+        //     sum1 = sum1+int(ch);
+        // }
+        // int sum2=0;
+        // for(int i=0;i<t.length();i++){
+        //     char chh=t[i];
+        //     sum2=sum2+char(chh);
+        // }
+        // int ans = sum2-sum1;
 
-        return char(ans);
+        // return char(ans);
+
+        // Approach 2 (XOR)
+        int ans=0;
+        for(char &ch : s){
+            ans= ans^ch;
+        }
+        for(char &ch :t){
+            ans=ans^ch;
+        }
+        return ans;
+
+
+
+
+
+
+
+        // Approach 3 (hashmap)
     }
 };

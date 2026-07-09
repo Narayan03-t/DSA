3ce60dcf11f50e1822708086d1e703208836e3cf
Tek Narayan Yadav <152596467+Narayan03-t@users.noreply.github.com>
2026-07-09 23:54:01 +0530

    Time: 2 ms (78.66%) | Memory: 8.3 MB (10.73%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,13 +1,39 @@
 class Solution {
 public:
-    int fib(int n) {
-        // base case
+    int solveUsingRecursion(int n){
+        // Recursive
+        // base case 
         if(n==0 || n==1){
             return n;
         }
 
         int ans = fib(n-1) + fib(n-2);
+        return ans;
+    }
 
+    int solveUsingMemo(int n,vector<int>& dp){
+        if(n==0 || n==1){
+            return n;
+        }
+        // step 3 -> base case ke baad if ans already exist or not
+        if(dp[n] != -1){
+            return dp[n];
+        }
+
+        // step 2 -> store and return recursion answer in dp array
+        dp[n] = solveUsingMemo(n-1,dp) + solveUsingMemo(n-2,dp);
+        return dp[n];
+    }
+    
+    int fib(int n) {
+        // int ans = solveUsingRecursion(n);
+        // return ans;
+
+        // DP
+        // step 1 -> create dp array and pass in function
+        vector<int>dp(n+1,-1);
+
+        int ans = solveUsingMemo(n,dp);
         return ans;
     }
 };

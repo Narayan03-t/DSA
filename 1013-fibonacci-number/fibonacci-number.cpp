be4523b90a2be7eb55c5c626d74d55f1887cb403
Tek Narayan Yadav <152596467+Narayan03-t@users.noreply.github.com>
2026-07-10 14:53:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (10.83%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -24,16 +24,39 @@ public:
         dp[n] = solveUsingMemo(n-1,dp) + solveUsingMemo(n-2,dp);
         return dp[n];
     }
+
+    int solveUsingTabulation(int n){
+        // step 1 -> create 1d/2d/3d DP array
+        vector<int>dp(n+1,-1);
+
+        // step 2 -> Analyze base case in recursive soln and update dp array accordingly
+        dp[0] = 0;
+        if(n==0){
+            return 0;
+        }
+        dp[1] = 1;
+
+        // step 3: -> check parameter and reverse it and run loop then copy paste logic in dp array and than return dp array with parameter(which is changing)
+        // recursion -> n->0
+        // reverse -> 0->n
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
     
     int fib(int n) {
         // int ans = solveUsingRecursion(n);
         // return ans;
 
-        // DP
+        // DP   TOP DOWN APPROACH (recursive)
         // step 1 -> create dp array and pass in function
-        vector<int>dp(n+1,-1);
+        // vector<int>dp(n+1,-1);
 
-        int ans = solveUsingMemo(n,dp);
+        // int ans = solveUsingMemo(n,dp);
+        // return ans;
+
+        int ans = solveUsingTabulation(n);
         return ans;
     }
 };

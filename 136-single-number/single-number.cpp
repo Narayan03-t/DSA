e1cf10c376800ce47e8632ab64d3f13cd01e6acd
Tek Narayan Yadav <152596467+Narayan03-t@users.noreply.github.com>
2026-07-12 16:57:02 +0530

    Time: 4 ms (20.68%) | Memory: 23.9 MB (10.55%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n=nums.size();
-        int ans=0;
-        for(int i=0;i<n;i++){
-            ans = ans ^ nums[i];
+        // // method 1 (XOR)
+        // int n=nums.size();
+        // int ans=0;
+        // for(int i=0;i<n;i++){
+        //     ans = ans ^ nums[i];
+        // }
+        // return ans;
+
+        // method 2
+        unordered_map<int,int>map;
+        int n = nums.size();
+        for(int i : nums){
+            map[i]++;
+        }
+        int ans=-1;
+        for(auto it : map){
+            if(it.second == 1){
+                ans = it.first;
+            }
         }
         return ans;
+
     }
 };

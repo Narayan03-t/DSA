5c08d6101fdf7bd08969b3564072390e30c7d000
Tek Narayan Yadav <152596467+Narayan03-t@users.noreply.github.com>
2026-07-13 14:34:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (23.40%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,18 +1,43 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-         int n = nums.size();
-        if (n == 0) return 0;
-        if (n == 1) return nums[0];
-        
-        vector<int> dp(n, 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-        
-        for (int i = 2; i < n; ++i) {
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i]);
+    int solvebyRec(vector<int>& nums,int n, int i){
+        // base case
+        if(i>=n){
+            return 0;
         }
-        
-        return dp[n-1];
+
+        int include = nums[i] + solvebyRec(nums,n,i+2);
+        int exclude = 0 + solvebyRec(nums,n,i+1);
+        int finalans = max(include,exclude);
+        return finalans;
+    }
+
+    int solveusingMemo(vector<int>& nums, vector<int>& dp,int n,int i){
+        // base case
+        if(i>=n){
+            return 0;
+        }
+
+        if(dp[i] != -1){
+            return dp[i];
+        }
+
+        int include = nums[i] + solveusingMemo(nums,dp,n,i+2);
+        int exclude = 0 + solveusingMemo(nums,dp,n,i+1);
+        dp[i] = max(include,exclude);
+        return dp[i];
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+
+        // int ans = solvebyRec(nums,n,i);
+        // return ans;
+
+        // step 1: create dp array and than pass in function
+        vector<int>dp(n+1,-1);
+        int ans = solveusingMemo(nums,dp,n,i);
+        return ans;
     }
 };

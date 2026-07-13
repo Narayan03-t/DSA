ac0aa630aa163e6e6a70ef52ee1975975ab51a7c
Tek Narayan Yadav <152596467+Narayan03-t@users.noreply.github.com>
2026-07-13 23:52:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (37.44%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // by Recursion
     int solve(int n){
         // base case
         if(n==0){
@@ -12,7 +13,7 @@ public:
         return ans;
 
     }
-
+    // solve by 1d dp -> memoization
     int solvebyMemo(int n,vector<int>& dp){
         // base case
         if(n==0){
@@ -28,12 +29,30 @@ public:
         return dp[n];
 
     }
-
+    // by tabulation
+    int solvebyTab(int n){
+        if(n==0){
+            return 1;
+        }
+        vector<int>dp(n+1);
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
+    }
     int climbStairs(int n) {
         //return solve(n);
         // 1d dp -> Top down approach
-        vector<int>dp(n+1,-1);
-        int ans = solvebyMemo(n,dp);
+        // vector<int>dp(n+1,-1);
+        // int ans = solvebyMemo(n,dp);
+        // return ans;
+
+        // bottom up approach -> Tabulation
+        int ans = solvebyTab(n);
         return ans;
     }
 };

822bb70fb222429788852a6b718504953c94493f
Tek Narayan Yadav <152596467+Narayan03-t@users.noreply.github.com>
2026-07-14 12:51:08 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (19.79%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,65 +1,54 @@
 class Solution {
 public:
-
-    void findfirstoccurence(vector<int>arr, int n, int target, int &firstindex){
-    
-    // Apply Binary search
-    int s = 0;
-    int e = n-1;
-    int mid = s + (e-s)/2 ;
-
-    while(s<=e){
-        if(arr[mid]==target){
-            firstindex = mid;
-            e = mid - 1;
-        }
-        if(target<arr[mid]){
-            e = mid - 1;
-        }
-        if(target>arr[mid]){
-            s = mid + 1;
+    void findlast(vector<int>& nums, int target,int s,int e,int n,int &last){
+        int mid = s + (e-s)/2;
+        while(s<=e){
+            if(nums[mid] == target){
+                last = mid;
+                s = mid+1;
+            }
+            if(target > nums[mid]){
+                s = mid+1;
+            }
+            if(target < nums[mid]){
+                e = mid-1;
+            }
+            mid = s + (e-s)/2;
         }
 
-        mid = s + (e-s)/2;
+        
     }
-}
-
-void lastoccurence(vector<int>arr, int n, int target, int &lastindex){
-    int s = 0;
-    int e = n-1;
-    int mid = s + (e-s)/2 ;
-
-    while(s<=e){
-        if(arr[mid]==target){
-            lastindex = mid;
-            s = mid + 1;
-        }
-        if(target<arr[mid]){
-            e = mid - 1;
-        }
-        if(target>arr[mid]){
-            s = mid + 1;
+    void findfirst(vector<int>& nums, int target,int s,int e,int n,int &first){
+        int mid = s + (e-s)/2;
+        while(s<=e){
+            if(nums[mid] == target){
+                first = mid;
+                e = mid-1;
+            }
+            if(target > nums[mid]){
+                s = mid+1;
+            }
+            if(target < nums[mid]){
+                e = mid-1;
+            }
+            mid = s + (e-s)/2;
         }
 
-        mid = s + (e-s)/2;
+        
     }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size();
+        int s = 0;
+        int e = n-1;
+        
 
-}
+        int first = -1;
+        int last = -1;
 
-    vector<int> searchRange(vector<int>& arr, int target) {
-        int n = arr.size();
-
-        int firstindex = -1;
-        int lastindex = -1;
-
-        findfirstoccurence(arr, n, target, firstindex);
-        lastoccurence(arr, n, target, lastindex);
-
-        vector<int>temp(2);
-        temp[0]= firstindex;
-        temp[1]= lastindex;
-        return temp;
+        
+        findfirst(nums,target,s,e,n,first);
+        findlast(nums,target,s,e,n,last);
+       
+        return {first,last};
     }
-    
-   
 };

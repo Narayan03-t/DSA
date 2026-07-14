0d3dda4f0975ba358ee177fe69eb39346490a50f
Tek Narayan Yadav <152596467+Narayan03-t@users.noreply.github.com>
2026-07-14 11:59:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (25.10%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -23,11 +23,30 @@ public:
         dp[n] = solveByMemo(n-1,dp) + solveByMemo(n-2,dp) + solveByMemo(n-3,dp);
         return dp[n];
     }
+    int solveByTab(int n){
+        if(n==0){
+            return 0;
+        }
+        if(n==1 || n==2){
+            return 1;
+        }
+        vector<int>dp(n+1,0);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+        return dp[n];
+    }
     int tribonacci(int n) {
         //return solve(n);
         // Top down approach Memoization( 1D dp)
-        vector<int>dp(n+1,-1);
-        int ans = solveByMemo(n,dp);
-        return ans;
+        // vector<int>dp(n+1,-1);
+        // int ans = solveByMemo(n,dp);
+        // return ans;
+
+        return solveByTab(n);
     }
 };

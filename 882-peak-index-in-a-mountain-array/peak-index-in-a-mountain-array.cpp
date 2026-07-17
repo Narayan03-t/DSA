e1b6ba45fb3388e899e0ecc5276fa6b3dfad0c57
Tek Narayan Yadav <152596467+Narayan03-t@users.noreply.github.com>
2026-07-18 01:13:20 +0530

    Time: 0 ms (100.00%) | Memory: 63.6 MB (48.45%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -13,22 +13,44 @@ public:
         // }
         // return index;
 
-
+        // method 2 Binary search store and compute
         int n = arr.size();
         int s = 0;
         int e = n-1;
         int mid = s + (e-s)/2;
-
-        while(s<e){
+        int ansIndex = -1;
+        while(s<=e){
             if(arr[mid] < arr[mid+1]){
                 // right me jaao
-                s = mid+1;
+                s = mid + 1;
             }
             else{
-                e = mid;
+                ansIndex = mid;
+                e = mid-1;
             }
-            mid = s + (e-s)/2;
+            mid  = s + (e-s)/2;
         }
-        return mid;
+        return ansIndex;
+
+
+
+        // method 3
+        // int n = arr.size();
+        // int s = 0;
+        // int e = n-1;
+        // int mid = s + (e-s)/2;
+
+        // while(s<e){
+        //     if(arr[mid] < arr[mid+1]){
+        //         // right me jaao (mtln line A pe hai jo strictly increasing order me hai)
+        //         s = mid+1;
+        //     }
+        //     else{
+        //          iss case me ans ho bhi skta hai nhi(line b me hai)
+        //         e = mid;
+        //     }
+        //     mid = s + (e-s)/2;
+        // }
+        // return mid;
     }
 };

c3599145651d64d3b24cd6b075b28640145ff8a3
Tek Narayan Yadav <152596467+Narayan03-t@users.noreply.github.com>
2026-07-18 00:41:19 +0530

    Time: 0 ms (100.00%) | Memory: 63.5 MB (48.40%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -1,52 +1,24 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-
-        int s = 0;
+        // brute force
+        int ans = INT_MIN;
+        int index = -1;
         int n = arr.size();
-        int e = n-1;
-        int mid = s + (e-s)/2;
-        int ansindex = -1;
-
-        while(s<=e){
-            if(arr[mid] < arr[mid+1]){
-                s = mid + 1;
+        for(int i=0;i<n;i++){
+            if(arr[i] > ans){
+                ans = arr[i];
+                index = i;
             }
-            else{
-                ansindex = mid;
-                e = mid-1;
-            }
-
-            mid = s+ (e-s)/2;
         }
+        return index;
+        // int n = arr.size();
+        // int s = 0;
+        // int e = n-1;
+        // int mid = s + (e-s)/2;
 
-        return ansindex;
-
-        // while(s<e){
-        //     if(arr[mid] < arr[mid + 1]){
-        //         s = mid + 1;  // increasing order line pe hai
-
-        //     }
-        //     else{
-        //         // decreasing order line pe hai
-        //         e = mid;
-        //     }
-
-        //     mid = s + (e-s)/2;
+        // while(s<=e){
+        //     if(arr[mid] < arr)
         // }
-
-        // return mid;
-
-        // int max = INT_MIN;
-        // int index = -1;
-
-        // for(int i=0;i<arr.size();i++){
-        //     if(arr[i]>max){
-        //         max = arr[i];
-        //         index = i;
-        //     }
-        // }
-        // return index;
-        
     }
 };

7efeba7369e59c8f9e23a171e253fd5a3e8bf14b
Tek Narayan Yadav <152596467+Narayan03-t@users.noreply.github.com>
2026-07-20 12:03:03 +0530

    Time: 7 ms (49.06%) | Memory: 13.6 MB (73.02%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        string ans = "";
         int n = s.length();
-        ans.push_back(s[0]);
-        
-        for(int i=1;i<n;i++){
-            char current = s[i];
+        string ans = "";
+        for(int i = 0;i<n;i++){
+            char curchar = s[i];
             if(ans.empty()){
-                ans.push_back(current);
+                ans.push_back(curchar);
             }
-            else if(current != ans.back()){
-                ans.push_back(current);
-            }
-            else if(current == ans.back()){
+            else if(ans.back() == curchar){
                 ans.pop_back();
             }
-
+            else{
+                ans.push_back(curchar);
+            }
         }
         return ans;
     }
